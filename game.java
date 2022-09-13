956b735ad0f38c081e613da2e1c50898a31f08c0
Agarwal786 <agarwalparth786@gmail.com>
2022-09-13 14:13:48 +0530

    new file:   game.java

---
+++ game.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+class game{
+    static int score=0;
+    public static void show_score()
+    {
+        System.out.println(game.score);
+    }
+    public static void start_game(int guess){
+        Random random=new Random();
+        int num=random.nextInt(101); 
+        if(num==guess)
+        {
+            game.score++;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int ch=0;
+        do{
+            System.out.println("Welcome to the Guessing Game");
+            System.out.println("1 to Start Game");
+            System.out.println("2 to Show Score");
+            System.out.println("3 to Exit");
+            ch=sc.nextInt();
+            switch(ch)
+            {
+                case 1:
+                System.out.print("Enter Your Guess(Between 1 to 100): ");
+                int guess=sc.nextInt();
+                start_game(guess);
+                break;
+                case 2:
+                show_score();
+                break;
+                case 3:
+                break;
+                default:
+                System.out.println("Incorrect Choice");
+                break;
+            }
+        }while(ch!=3);
+    }
+}
